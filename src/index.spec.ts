b4b756815dcecc553781649e95cf8e7a477a7f9e
Vladislav Mamon <norskeld@gmail.com>
2022-11-23 03:15:47 +0300

    feat: initial implementation

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,17 +1,111 @@
 import { describe, it, expect } from 'vitest'
 
-import { identity } from './index'
+import un from './index'
 
-describe('index.ts', () => {
-  it('given a primitive, should return the same primitive', async () => {
-    expect(await identity('string')).toBe('string')
-    expect(await identity(1986)).toBe(1986)
-    expect(await identity(true)).toBe(true)
-    expect(await identity(undefined)).toBe(undefined)
+function concat(...segments: Array<string>): string {
+  return segments.join('\n') + '\n'
+}
+
+describe('un (tagged)', () => {
+  it('should unindent if given a multiline string', () => {
+    const actual = un`
+        begin
+          - indented line
+          - indented line
+        end
+    `
+
+    // prettier-ignore
+    const expected = concat(
+      'begin',
+      '  - indented line',
+      '  - indented line',
+      'end'
+    )
+
+    expect(actual).toBe(expected)
   })
 
-  it('given an object, should return the same object', async () => {
-    const value = { foo: 'bar' }
-    expect(await identity(value)).toBe(value)
+  it('should unindent if given a multiline string that starts right after quote/backtick', () => {
+    const actual = un`begin
+                        - indented line
+                        - indented line
+                      end
+    `
+
+    // prettier-ignore
+    const expected = concat(
+      'begin',
+      '  - indented line',
+      '  - indented line',
+      'end'
+    )
+
+    expect(actual).toBe(expected)
+  })
+
+  it('should return as-is if given a single-line string', () => {
+    const actual = un`first`
+    const expected = `first`
+
+    expect(actual).toBe(expected)
+  })
+
+  it('should throw if provided with not a string or template strings array', () => {
+    // Creating bare object that doesn't have any means in the prototype chain to let it be
+    // converted into string (toString/toPrimitive).
+    const bare = Object.create(null)
+
+    expect(() => un`${bare as unknown as string}`).toThrow()
+  })
+})
+
+describe('un (untagged)', () => {
+  it('should unindent given multiline string', () => {
+    const actual = un(`
+        begin
+          - indented line
+          - indented line
+        end
+    `)
+
+    // prettier-ignore
+    const expected = concat(
+      'begin',
+      '  - indented line',
+      '  - indented line',
+      'end'
+    )
+
+    expect(actual).toBe(expected)
+  })
+
+  it('should unindent if given a multiline string that starts right after quote/backtick', () => {
+    const actual = un(`begin
+                        - indented line
+                        - indented line
+                      end
+    `)
+
+    // prettier-ignore
+    const expected = concat(
+      'begin',
+      '  - indented line',
+      '  - indented line',
+      'end'
+    )
+
+    expect(actual).toBe(expected)
+  })
+
+  it('should return as-is if given a single-line string', () => {
+    const actual = un('first')
+    const expected = 'first'
+
+    expect(actual).toBe(expected)
+  })
+
+  it('should throw if provided with not a string or template strings array', () => {
+    expect(() => un({} as unknown as string)).toThrow()
   })
 })
